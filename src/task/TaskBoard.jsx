da3ed0244ac55afda45ad9b354635ae74c7bd9e3
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2024-08-19 01:58:31 +0600

    favorite and unfavorite section added

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -56,6 +56,15 @@ const TaskBoard = () => {
     setTasks([...tasks]);
   }
 
+  const handleFavorite = (taskId) => {
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+    const newTasks = [...tasks];
+
+    newTasks[taskIndex].isFavorite = !newTasks[taskIndex].isFavorite;
+    setTasks(newTasks);
+  }
+
   return (
     <section className="mb-20" id="tasks">
       {showAddModal && <AddTaskModal onSave={handleAddEditTask} onCloseClick={handleCloseClick} taskToUpdate={taskToUpdate} />}
@@ -65,7 +74,7 @@ const TaskBoard = () => {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions onAddClick={() => setShowAddModal(true)} onDeleteAllClick={handleDeleteAllClick} />
-          <TaskList tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask} />
+          <TaskList tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask} onFav={handleFavorite} />
         </div>
       </div>
     </section>
--- src/task/TaskList.jsx
+++ src/task/TaskList.jsx
@@ -1,6 +1,6 @@
 import { FaStar } from "react-icons/fa";
 
-const TaskList = ({ tasks, onEdit, onDelete }) => {
+const TaskList = ({ tasks, onEdit, onDelete, onFav }) => {
   return (
     <div className="overflow-auto">
       <table className="table-fixed overflow-auto xl:w-full">
@@ -36,7 +36,13 @@ const TaskList = ({ tasks, onEdit, onDelete }) => {
               className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2"
             >
               <td>
-                {task.isFavorite ? <FaStar color="yellow" />: <FaStar color="gray" />}
+                <button onClick={() => onFav(task.id)}>
+                  {task.isFavorite ? (
+                    <FaStar color="yellow" />
+                  ) : (
+                    <FaStar color="gray" />
+                  )}
+                </button>
               </td>
               <td>{task.title}</td>
               <td>
@@ -56,8 +62,18 @@ const TaskList = ({ tasks, onEdit, onDelete }) => {
               <td className="text-center">{task.priority}</td>
               <td>
                 <div className="flex items-center justify-center space-x-3">
-                  <button className="text-red-500" onClick={() => onDelete(task.id)} >Delete</button>
-                  <button className="text-blue-500" onClick={() => onEdit(task)}>Edit</button>
+                  <button
+                    className="text-red-500"
+                    onClick={() => onDelete(task.id)}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    className="text-blue-500"
+                    onClick={() => onEdit(task)}
+                  >
+                    Edit
+                  </button>
                 </div>
               </td>
             </tr>
